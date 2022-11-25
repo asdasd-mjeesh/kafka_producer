1f940521ecafca9c2531c613fcaa1b12380f49c0
asdasd-mjeesh <201300@nuos.edu.ua>
2022-11-25 16:48:59 +0200

    upload project

---
+++ src/main/java/com/javamaster/producer/ProducerService.java
@@ -0,0 +1,19 @@
+package com.javamaster.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProducerService {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public ProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void produce(String message) {
+        kafkaTemplate.send("message", message);
+    }
+}
---
+++ src/main/java/com/javamaster/rest_controller/ControllerV1.java
@@ -0,0 +1,22 @@
+package com.javamaster.rest_controller;
+
+import com.javamaster.producer.ProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/controller/api/v1")
+public class ControllerV1 {
+    private final ProducerService producerService;
+
+    @Autowired
+    public ControllerV1(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @GetMapping("/generate")
+    public String generate(@RequestParam String message) {
+        producerService.produce(message);
+        return "OK";
+    }
+}
